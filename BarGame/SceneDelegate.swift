003a37abfb36c78483ad62c1679eb6c80dd83d65
Willy <willy548798@gmail.com>
2026-08-08 15:15:53 +0800

    feat: 全域設定深色模式

--- BarGame/SceneDelegate.swift
+++ BarGame/SceneDelegate.swift
@@ -15,6 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = scene as? UIWindowScene else { return }
 
         let window = UIWindow(windowScene: windowScene)
+        window.overrideUserInterfaceStyle = .dark
         window.rootViewController = MainTabBarController()
         window.makeKeyAndVisible()
         self.window = window
